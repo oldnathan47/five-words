56c6556f163a6090f785f6f2b16cec3f28c4c91f
Neil Patel <nepatel@davidson.edu>
2019-08-13 16:18:52 -0700

    set up adonis

---
+++ five-words/start/routes.js
@@ -0,0 +1,30 @@
+"use strict";
+
+/*
+|--------------------------------------------------------------------------
+| Routes
+|--------------------------------------------------------------------------
+|
+| Http routes are entry points to your web application. You can create
+| routes for different URL's and bind Controller actions to them.
+|
+| A complete guide on routing is available here.
+| http://adonisjs.com/docs/4.1/routing
+|
+*/
+
+/** @type {typeof import('@adonisjs/framework/src/Route/Manager')} */
+const Route = use("Route");
+
+// landing page
+Route.on("/").render("landing");
+
+// directory page
+
+// input page
+
+// data page
+
+// sign-up page
+
+// sign-in page
